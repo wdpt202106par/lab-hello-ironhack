8da98d50b193aa85836f9453c968153a50e363db
Laura Kapitza <laura.kapitza@gmail.com>
2021-06-18 23:49:34 +0200

    fix position of JSX code

--- src/app.js
+++ src/app.js
@@ -1,5 +1,41 @@
 import React from "react";
+
+
 const App = () => {
-  return <h1 className="app">Hello World!</h1>;
+  return (
+    <div>
+      <section>
+        <img src="/images/ironhack-logo.svg" />
+        <h1>Say hello to ReactJS</h1>
+        <p>You will learn how to use the most popular frontend library,and become a super ninja developer</p>
+        <button>Awesome!</button>
+      </section>
+
+      <section>
+        <article>
+          <img src="/images/icon1.png" />
+          <h2>Declarative</h2>
+          <p>react makes it painless to create interactive UIs</p>
+        </article>
+        <article>
+          <img src="/images/icon2.png" />
+          <h2>Componenets</h2>
+          <p>Build encapsulated components that manage their state.</p>
+        </article>
+        <article>
+          <img src="/images/icon3.png" />
+          <h2>Single-way</h2>
+          <p>A set of immutable values are passed to the component's.</p>
+        </article>
+        <article>
+          <img src="/images/icon4.png" />
+          <h2>JSX</h2>
+          <p>Statically-typed,desiegned to run on moders browsers.</p>
+        </article>
+      </section>
+    </div>
+  )
 };
+
+
 export default App;
